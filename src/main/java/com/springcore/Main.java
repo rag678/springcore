9ddf363e0aade8904fa9b4e6ac11cc071e7b97b9
Anurag <anurag.baranwal@Kuliza.com>
2023-06-18 15:22:22 +0530

    adding dependency using config classPath

---
+++ src/main/java/com/springcore/Main.java
@@ -0,0 +1,25 @@
+package com.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
+        Student student1 = (Student) applicationContext.getBean("student1");
+        System.out.println(student1);
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+//        for (int i = 1; i <= 5; i++) {
+//
+//            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+//            // for you, but you can always add more by pressing Ctrl+F8.
+//            System.out.println("i = " + i);
+//        }
+    }
+}

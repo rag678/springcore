9a36fd131a0a15ad93b47ad391eff209e411675f
rag678 <87017032+rag678@users.noreply.github.com>
2023-06-19 06:21:28 +0530

    Revert "injecting collection to spring"

--- src/main/java/com/springcore/Student.java
+++ src/main/java/com/springcore/Student.java
@@ -16,22 +16,18 @@ public class Student {
     }
 
     public int getStudentId() {
-        System.out.println("calling get id");
         return studentId;
     }
 
     public void setStudentId(int studentId) {
-        System.out.println("calling set id");
         this.studentId = studentId;
     }
 
     public String getStudentName() {
-        System.out.println("calling get name");
         return studentName;
     }
 
     public void setStudentName(String studentName) {
-        System.out.println("calling set name");
         this.studentName = studentName;
     }
 

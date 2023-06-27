158d4950cd5b4430b1d02ebed6f8bef14f4e5cb8
Anurag <anurag.baranwal@Kuliza.com>
2023-06-27 15:53:13 +0530

    inject reference

---
+++ src/main/java/com/springcore/ref/B.java
@@ -0,0 +1,23 @@
+package com.springcore.ref;
+
+public class B {
+    private Integer y;
+
+    public B() {
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "B{" +
+                "y=" + y +
+                '}';
+    }
+}

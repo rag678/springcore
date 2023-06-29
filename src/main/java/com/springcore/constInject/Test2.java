cd3b2602371a5c13018fb00db50b5584bdcfd46c
Anurag <anurag.baranwal@Kuliza.com>
2023-06-29 13:19:02 +0530

    without any message

--- src/main/java/com/springcore/constInject/Test2.java
+++ src/main/java/com/springcore/constInject/Test2.java
@@ -1,17 +1,12 @@
 package com.springcore.constInject;
 
-import com.springcore.ref.A;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test2 {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("refconfig.xml");
-        A temp =(A) context.getBean("aref");
-        System.out.println(temp.getX());
-
-        System.out.println(temp.getObj().getY());
-
-        System.out.println(temp);
+        ApplicationContext context = new ClassPathXmlApplicationContext("ciconfig.xml");
+        Person per = (Person) context.getBean("person");
+        System.out.println(per);
     }
 }
